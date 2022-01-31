39b57fdbd41c6e9a8e5bbaf5ec68210975c6ce97
Dhananjay <dhananjayrakshe77@gmail.com>
2022-02-01 01:36:37 +0530

    Drag & drop wirframe ready

--- src/App.js
+++ src/App.js
@@ -1,23 +1,19 @@
-import logo from './logo.svg';
-import './App.css';
+import grapesjs from "grapesjs";
+import { useState, useEffect } from "react";
 
 function App() {
+
+  const [editor, setEditor] = useState(null);
+
+  useEffect(() => {
+   const editor = grapesjs.init({
+     container: "#editor"
+   });
+   setEditor(editor);
+  }, []);
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <div id="editor"></div>
     </div>
   );
 }
